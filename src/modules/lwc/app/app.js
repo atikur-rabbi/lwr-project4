625e90a0e1a2c053470826c19ea7084c56fe254b
atikur-rabbi <msawon94@gmail.com>
2022-05-06 11:27:17 +0600

    update

---
+++ src/modules/lwc/app/app.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloWorldApp extends LightningElement {
+    title = "Welcome to Lightning Web Components!";
+    clickedButtonLabel;
+
+    handleClick(event) {
+        this.clickedButtonLabel = event.target.label;
+
+        console.log('clickedButtonLabel: ' + this.clickedButtonLabel);
+    }
+}
